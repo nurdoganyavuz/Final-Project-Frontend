c5d8aac2dc15ae66599d708fbe7b5022e74d7d6a
NURDOGAN YAVUZ <nurdogan.yavuz@ogrenci.karatay.edu.tr>
2021-03-21 23:11:59 +0300

    Updated - Day 18

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/models/category';
+import { CategoryService } from 'src/app/services/category.service';
+
 
 @Component({
   selector: 'app-category',
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryComponent implements OnInit {
 
-  constructor() { }
+  //yöneteceğimiz dataları her zaman burada tanımlarız. Çünkü html bunlar üzerinden ilerlicek.
+  categories : Category[] = [];
+  currentCategory : Category | null; //arkaplanda newlenip instance'ının olusturulması gerekiyor. Bu yüzden tsconfig dosyasına gerekli kodu eklememiz gerekir.
+
+  constructor(private categoryService:CategoryService) { }
 
   ngOnInit(): void {
+    this.getCategories();
+  }
+
+  getCategories(){
+    this.categoryService.getCategories().subscribe(response=>{
+      this.categories = response.data;
+    })
+    
+  }
+  setCurrentCategory(category:Category){
+    this.currentCategory = category;
+  }
+  setAllCategoryNull(){
+    this.currentCategory = null //"tüm ürünler" sekmesini seçtiğimizde currentcategory sıfırlansın istiyoruz.
+  }
+  getCurrentCategoryClass(category:Category){
+    if(category == this.currentCategory){
+      return "list-group-item active"
+    }else{
+      return "list-group-item"
+    }
+  }
+
+  getAllCategoryClass(){
+    if(!this.currentCategory){
+      return "list-group-item active"
+    }else{
+      return "list-group-item"
+    }
   }
 
 }
